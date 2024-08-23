a20d4419edd7b355f39205c2f5127090c6e14b41
Dayncod <132489003+Dayncod@users.noreply.github.com>
2024-08-23 21:39:19 +0300

    -

--- Bot add roles discord.py
+++ Bot add roles discord.py
@@ -4,13 +4,17 @@ from discord.ext import commands
 
 config = {
     'token': 'MTI3NjE1ODg3OTk0NzgxNjk3MA.GqVFWS.DemFprpAYtsfaEpUeG4aWH_Q0i0CKe_k34aPis',
-    'prefix': '$',
+    'prefix': '/',
 }
 
 bot = commands.Bot(config['prefix'], intents=discord.Intents.all())
 
 @bot.command()
-async def rand(ctx, *arg):
-    await ctx.reply(random.randint(0, 100))
+async def start(ctx):
+    roles = ctx.guild.get_member(bot.user.id)
+    rolesTring = ''
+    for role in roles.roles:
+        rolesTring += str(role) + ', '
+    await ctx.reply(f'Мои роли {rolesTring}')
 
 bot.run(config['token'])
