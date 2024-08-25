3ed2b8eeff34554b46a88a46f52aae540a420557
Dayncod <132489003+Dayncod@users.noreply.github.com>
2024-08-25 13:05:21 +0300

    -

--- Bot add roles discord.py
+++ Bot add roles discord.py
@@ -9,17 +9,28 @@ config = {
 
 bot = commands.Bot(config['prefix'], intents=discord.Intents.all())
 
-@bot.command()
-# async def start(ctx):
-#     roles = ctx.guild.get_member(bot.user.id)
-#     rolesTring = ''
-#     author = ctx.message.author
-#     for role in roles.roles:
-#         rolesTring += str(role) + ', '
-#     await ctx.reply(f'Привет {author.mention}, мои роли {rolesTring}')
+# @bot.event
 
-async def appoint(ctx):
-    role = None
-    await ctx.reply(f'Пользователь {ctx.message.author.mention} назначен на должность {role}')
+# async def on_member_join(member):
+
+
+
+@bot.command()
+async def bot_roles_list(ctx):
+    roles = ctx.guild.get_member(bot.user.id)
+    rolesTring = ''
+    author = ctx.message.author
+    for role in roles.roles:
+        rolesTring += str(role) + ', '
+    await ctx.reply(f'Привет {author.mention}, мои роли {rolesTring}')
+
+@bot.command()
+async def appoint(ctx, user:discord.Member):
+    defaultRole = ctx.guild.get_role(1273537192391868460)
+    if (defaultRole in user.roles):
+        await ctx.reply(f'Пользователь {user.mention} уже назначен на роль {defaultRole.mention}')
+    else:
+        await user.add_roles(defaultRole)
+        await ctx.reply(f'Пользователь {user.mention} назначен на должность {defaultRole.mention}')
 
 bot.run(config['token'])
