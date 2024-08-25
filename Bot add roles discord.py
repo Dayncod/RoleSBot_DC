aa00abc2b5d6e3086aec3d8e00a210f285e2aed7
Dayncod <132489003+Dayncod@users.noreply.github.com>
2024-08-25 11:40:17 +0300

    -

--- Bot add roles discord.py
+++ Bot add roles discord.py
@@ -4,17 +4,22 @@ from discord.ext import commands
 
 config = {
     'token': 'MTI3NjE1ODg3OTk0NzgxNjk3MA.GqVFWS.DemFprpAYtsfaEpUeG4aWH_Q0i0CKe_k34aPis',
-    'prefix': '/',
+    'prefix': '$',
 }
 
 bot = commands.Bot(config['prefix'], intents=discord.Intents.all())
 
 @bot.command()
-async def start(ctx):
-    roles = ctx.guild.get_member(bot.user.id)
-    rolesTring = ''
-    for role in roles.roles:
-        rolesTring += str(role) + ', '
-    await ctx.reply(f'Мои роли {rolesTring}')
+# async def start(ctx):
+#     roles = ctx.guild.get_member(bot.user.id)
+#     rolesTring = ''
+#     author = ctx.message.author
+#     for role in roles.roles:
+#         rolesTring += str(role) + ', '
+#     await ctx.reply(f'Привет {author.mention}, мои роли {rolesTring}')
+
+async def appoint(ctx):
+    role = None
+    await ctx.reply(f'Пользователь {ctx.message.author.mention} назначен на должность {role}')
 
 bot.run(config['token'])
