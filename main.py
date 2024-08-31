0d59353188a5d8ece3c76b66acfd2739799d1b57
Dayncod <132489003+Dayncod@users.noreply.github.com>
2024-09-01 00:18:23 +0300

    -

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ config = {
 
 bot = commands.Bot(config['prefix'], intents=discord.Intents.all())
 
-defaultRole = get(client.get_all_channels())
 
 @bot.event
 async def on_member_join(member):
@@ -20,8 +19,8 @@ async def on_member_join(member):
     await chanal.send(f'Добро пожаловать на сервер, {member.mention}, ты был назначен на роль {defaultRole.mention}')
 
 @bot.command()
-async def appoint(ctx, user:discord.Member, defaultRole):
-    # defaultRole = ctx.guild.get_role(1273537192391868460)
+async def appoint(ctx, user:discord.Member):
+    defaultRole = ctx.guild.get_role(1273537192391868460)
     if (defaultRole in user.roles):
         await ctx.send(f'Пользователь {user.mention} уже назначен на роль {defaultRole.mention}')
     else:
